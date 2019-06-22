9b84c95066a79478ea9425d7f0d6bd0d6fb9f101
esctaticdivyansh <47699851+esctaticdivyansh@users.noreply.github.com>
2019-06-23 03:54:49 +0530

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,21 @@
-let video=document.getElementById('me');
 let handleFail = function(err){
     console.log("Error : ", err);
 };
-let remoteContainer= document.getElementById("remote-container");
+const video = document.getElementById('me')
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
-    ]).then(addVideoStream) 
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(addVideoStream)
+let remoteContainer= document.getElementById("remote-container");
 function addVideoStream(streamId){
     let streamDiv=document.createElement("div"); // Create a new div for every stream
     streamDiv.id=streamId;                       // Assigning id to div
     streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
     remoteContainer.appendChild(streamDiv);      // Add new div to container
 }
-function removeVideoStream (evt) {
+function removeVideoStream (evt){
     let stream = evt.stream;
     stream.stop();
     let remDiv=document.getElementById(stream.getId());
@@ -34,27 +34,19 @@ client.join(null,"any-channel",null, (uid)=>{
         video: true,
         screen: false
     });
-
-    // Associates the stream to the client
     localStream.init(function() {
-
-        //Plays the localVideo
         localStream.play('me');
-        //Publishes the stream to the channel
         client.publish(localStream, handleFail);
-
     },handleFail);
-//When a stream is added to a channel
+},handleFail);
 client.on('stream-added', function (evt) {
     client.subscribe(evt.stream, handleFail);
 });
-//When you subscribe to a stream
 client.on('stream-subscribed', function (evt) {
     let stream = evt.stream;
     addVideoStream(stream.getId());
     stream.play(stream.getId());
 });
-//When a person is removed from the stream
 client.on('stream-removed',removeVideoStream);
 client.on('peer-leave',removeVideoStream);
 video.addEventListener('play', () => {
